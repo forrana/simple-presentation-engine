dd01e5cf337e36e4fdc4170adb2e5c911fffeb21
forrana <forrana@gmail.com>
2017-04-09 00:25:08 +0300

    Flickr integration * Add Service for integration with flikcr

---
+++ src/app/services/flickr.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class FlickrService {
+  private API_KEY = '8e2e23136a5f26f8a2d2e414a9f6dcff';
+  private imagesUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.API_KEY}&tags=`;  // URL to web API
+
+  constructor (private http: Http) {}
+
+   getImagesByTags(tags): Observable<any[]> {
+       return this.http.get(this.imagesUrl + tags)
+            //         .map(this.extractData)
+                     .catch(this.handleError);
+   }
+
+   private extractData(res: Response) {
+     let body = res.json();
+     return body.data || { };
+   }
+   private handleError (error: Response | any) {
+     // In a real world app, you might use a remote logging infrastructure
+     let errMsg: string;
+     if (error instanceof Response) {
+       const body = error.json() || '';
+       const err = body.error || JSON.stringify(body);
+       errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+     } else {
+       errMsg = error.message ? error.message : error.toString();
+     }
+     console.error(errMsg);
+     return Observable.throw(errMsg);
+   }
+
+}
